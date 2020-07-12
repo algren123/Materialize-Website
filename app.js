ab7d646310cdcf7c4031a61296acedda225883c0
Algren Pauna <algren.pauna@gmail.com>
2020-07-12 21:01:03 +0100

    implemented Google Maps API

---
+++ app.js
@@ -0,0 +1,17 @@
+function initMap() {
+    let location = {
+        lat: 52.070348,
+        lng: -0.51732,
+    }
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: location,
+    });
+
+    let marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
